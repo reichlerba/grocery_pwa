4d5917b45cf765b0c9630362a30952530c36496b
Benjamin Reichler <reichlerba@gmail.com>
2026-05-16 09:37:05 -0700

    updated cache logic to delete old caches upon update

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,18 @@
 //  PWA apps cache files on first load, from the Service Worker, and later only serves cached version
 
-const CACHE_NAME = "grocery-app-1.0";
+const CACHE_NAME = "grocery-app-1.1";
 
 const FILES = [
     "./",
     "./index.html",
     "./manifest.json",
     "./style.css",
-    "./app.js"
+    "./app.js",
+    "./icons/icon-256.png"
 ];
 
 self.addEventListener("install", e => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(FILES);
@@ -22,6 +24,20 @@ self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(res => {
             return res || fetch(e.request);
+        })
+    );
+});
+
+// upon downloading new app update (new cache), delete old cache
+self.addEventListener("activate", event => {
+    clients.claim();
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
         })
     );
 });
